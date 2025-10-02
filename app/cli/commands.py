f8c6c6907aa8d97bd25c45b4f9b25b51c74c7c0c
RJ Bergeron <rj@arrjay.net>
2025-10-01 22:06:28 -0400

    provide ability to configure the streaming mode, fix argument for https setup

--- app/cli/commands.py
+++ app/cli/commands.py
@@ -2,11 +2,23 @@ import db.settings
 from cli.registry import command
 
 
-@command('streaming-mode')
-def streaming_mode(_args):
+@command('show-streaming-mode')
+def show_streaming_mode(_args):
     """Prints the currently applicable streaming mode."""
     mode = db.settings.Settings().get_streaming_mode().value
     print(mode)
+
+@command('set-streaming-mode')
+def set_streaming_mode(_args):
+    """Stores the preferred streaming mode."""
+    try:
+        mode = _args[1]
+        if db.settings.StreamingMode(mode):
+            db.settings.Settings().set_streaming_mode(db.settings.StreamingMode(mode))
+        else:
+            raise ValueError("argument not a valid streaming mode")
+    except Exception as e:
+        raise e
 
 @command('show-https-mode')
 def show_https_mode(_args):
@@ -18,7 +30,7 @@ def show_https_mode(_args):
 def set_https_mode(_args):
     """Configures if TinyPilot requires HTTPS. Takes a string, interpolates as boolean."""
     try:
-        setting = _args[0]
+        setting = _args[1]
         if setting == True:
             db.settings.Settings().set_requires_https(1)
         else:
