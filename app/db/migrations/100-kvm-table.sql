cea83253e8e8e710e8ced9392d9314513bbc513f
RJ Bergeron <rj@bbxn.us>
2026-03-11 21:12:23 -0400

    add support for an IR-controlled KVM from Amazon. it's under a couple different sellers.

--- app/db/migrations/100-kvm-table.sql
+++ app/db/migrations/100-kvm-table.sql
@@ -10,7 +10,7 @@
 CREATE TABLE IF NOT EXISTS external_kvm(
     id INTEGER PRIMARY KEY,
     codename TEXT NOT NULL UNIQUE,
-    label TEXT NOT NULL UNIQUE,
+    label TEXT NOT NULL,
     ports INTEGER NOT NULL,
     portscript TEXT,
     commandscript TEXT
@@ -37,3 +37,11 @@ VALUES
   (NULL, (SELECT id FROM external_kvm WHERE codename = 'aten_cs'), 'sync-edid', 'Sync Monitor EDID'),
   (NULL, (SELECT id FROM external_kvm WHERE codename = 'aten_cs'), 'toggle-mouseemu', 'Toggle Mouse Emulation'),
   (NULL, (SELECT id FROM external_kvm WHERE codename = 'aten_cs'), 'report', 'Report KVM Configuration');
+
+-- notice this has no portscript, commandscript does it all
+INSERT OR IGNORE INTO external_kvm(id, codename, label, ports, commandscript)
+VALUES (NULL, 'vpfet-ir', 'IR KVM', 0, '/usr/lib/tinypilot/scripts/vpfet-control');
+
+INSERT OR IGNORE INTO external_kvm_commands(id, kvm_id, action, label)
+VALUES
+  (NULL, (SELECT id FROM external_kvm WHERE codename = 'vpfet-ir'), 'reset', 'Reset KVM'),
